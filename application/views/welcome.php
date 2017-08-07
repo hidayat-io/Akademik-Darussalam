0141e0b2f8e4ac96e2cc05352e5a3d2d52e390f0
faritno <faritnoz1007@live.com>
2017-08-07 21:29:04 +0700

    design intro
    perubahan tulisan design intro

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,11 +1,12 @@
 <!-- Content Header (Page header) -->
 <center>
 <section class="content-header">
-  	<div style="vertical-align: middle;">
-  		<img style="width: 200px" src="<?php echo base_url() ?>/assets/images/logo.png" alt="docs-logo" /><br><br>
-  		<h3>Selamat Datang di</h3>
-  		<div><h1><b>ALTIS</b></h1><h2>Sistem</h2></div>
-  		<h3>Akademik TMI Darussalam</h3>
+  	<div style="vertical-align: middle;padding-top:100px;">
+  		<img style="width: 200px" src="http://pesantren.hidayat.info//assets/images/logo.png" alt="docs-logo"><br><br>
+  		<h3 style="font-size:50px">Selamat Datang di</h3>
+  		<div><h1 style="font-size:150px"><b>ALTIS</b>
+</h1><h2 style="font-size:60px; letter-spacing:35px;padding-left:15px; line-height:10px;">SYSTEM</h2></div>
+  		<h3 style="font-size:45px; letter-spacing:0px;padding-left:15px; line-height:150px;">Akademik TMI Darussalam</h3>
   	</div>	
 </section>
 </center>
