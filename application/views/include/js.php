78433d644bbd184043059d3bb88d84e843362884
adongIO <adongfranciskus@yahoo.co.id>
2017-12-23 21:42:48 +0700

    update js
    js

--- application/views/include/js.php
+++ application/views/include/js.php
@@ -17,6 +17,10 @@
 <script src="<?php echo base_url(); ?>assets/plugins/datatables/datatables.min.js" type="text/javascript"></script>
 <script src="<?php echo base_url(); ?>assets/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js" type="text/javascript"></script>
 <script src="<?php echo base_url(); ?>assets/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js" type="text/javascript"></script>
+
+<script src="<?php echo base_url(); ?>assets/plugins/bootstrap-daterangepicker/daterangepicker.js" type="text/javascript"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/bootstrap-daterangepicker/daterangepicker.min.js" type="text/javascript"></script>
+
 <script src="<?php echo base_url(); ?>assets/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
 <script src="<?php echo base_url(); ?>assets/plugins/jquery-validation/js/additional-methods.min.js" type="text/javascript"></script>
 <script src="<?php echo base_url(); ?>assets/plugins/bootbox/bootbox.min.js" type="text/javascript"></script>
