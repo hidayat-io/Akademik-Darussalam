058bf91f33c7769a42bcb64c4d7fc4105a9ff657
DjongFaritno <faritnoz1007@live.com>
2018-05-28 10:39:35 +0700

    gitignore

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/akademik-pesantren';
+$config['base_url'] = 'http://10.12.0.223/akademik-pesantren';
 
 /*
 |--------------------------------------------------------------------------
